9c886410f6b61944607a35a1d637c910d3dabe17
eojinkim1 <rladjwls999@gmail.com>
2024-05-17 23:37:08 +0900

    카드 생성 클래스 생성

--- src/main/java/step_1/Card.java
+++ src/main/java/step_1/Card.java
@@ -1,4 +1,22 @@
 package step_1;
 
-public enum Card {
+public class Card {
+    private final String SUIT;
+    private final int NUMBER;
+
+    public Card(String suit, int number) {
+        this.SUIT = suit;
+        this.NUMBER = number;
+    }
+    public String getSUIT(){
+        return SUIT;
+    }
+    public int getNUMBER(){
+        return NUMBER;
+    }
+
+    @Override
+    public String toString() {
+        return SUIT + NUMBER;
+    }
 }
