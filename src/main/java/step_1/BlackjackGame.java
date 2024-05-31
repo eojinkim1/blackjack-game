7fa4bda16e9cc30e9be51ea3715eb86b2c82c55f
eojinkim1 <rladjwls999@gmail.com>
2024-05-31 23:08:49 +0900

    블랙잭 게임 완성

--- src/main/java/step_1/BlackjackGame.java
+++ src/main/java/step_1/BlackjackGame.java
@@ -1,33 +1,90 @@
 package step_1;
 
-import java.util.List;
-
+import java.util.Scanner;
 public class BlackjackGame {
-    public static void main(String[] args) {
-        printDeck();
-//        System.out.println("게임을 시작합니다.");
-//
-//        System.out.println("딜러 카드 : ");
-//        System.out.println("플레이어 카드 : ");
-//
-//        System.out.println("행동을 결정해주세요. Hit or Stay? (H/S): ");
-//
-//
-//        System.out.println("승리했습니다!");
-//        System.out.println("패배했습니다ㅠ");
-//
-//        System.out.println("게임을 다시 시작하시겠습니까? ( Y / N )");
-//        System.out.println("새로운 게임을 시작합니다!");
-//
-//        System.out.println("게임이 종료되었습니다.");
+    private Deck deck = new Deck();
+    private Player player = new Player();
+    private Dealer dealer = new Dealer();
 
+    public static void main(String[] args) {
+        BlackjackGame game = new BlackjackGame();
+        game.startGame();
+    }
+    public void startGame() {
+        System.out.println("게임을 시작합니다.");
+        dealCards();
+        statusDisplay();
+
+        play();
+
+        if (!player.isBusted()){
+            dealer.drawCard(deck);
+        }
+
+        showFinalHands();
+        determineWinner();
     }
 
-    private static void printDeck() {
-        Deck deck = new Deck();
-        List<Card> cards = deck.getCards();
-        for (Card card : cards) {
-            System.out.println(card);
+    private void dealCards() {
+        player.addCard(deck.drawCard());
+        dealer.addCard(deck.drawCard());
+        player.addCard(deck.drawCard());
+        dealer.addCard(deck.drawCard());
+    }
+
+    private void statusDisplay() {
+        System.out.println("플레이어 카드 : " + player);
+        System.out.println("딜러 카드 : " + dealer.toStringWithHiddenCard());
+    }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("행동을 결정해주세요. Hit or Stay? (H/S): ");
+            String action = scanner.next().toUpperCase();
+
+            if (action.equals("H")) {
+                player.addCard(deck.drawCard());
+                if (player.isBusted()) {
+                    System.out.println("플레이어 버스트!");
+                    break;
+                }
+                dealer.drawCard(deck);
+                    statusDisplay();
+                if (dealer.isBusted()){
+                    System.out.println("딜러 버스트!");
+                    break;
+                }
+            } else if (action.equals("S")) {
+                break;
+            } else {
+                System.out.println("잘못된 입력입니다. 'H'를 입력해서 히트하거나 'S'를 입력해서 스테이하세요.");
+            }
+        }
+    }
+
+    private void showFinalHands() {
+        System.out.println("카드를 오픈합니다.");
+        System.out.println("플레이어 카드 : " + player);
+        System.out.println("플레이어 카드 합계: " + player.getHandValue());
+        System.out.println("딜러 카드 : " + dealer);
+        System.out.println("딜러 카드 합계: " + dealer.getHandValue());
+    }
+
+    private void determineWinner() {
+        int playerValue = player.getHandValue();
+        int dealerValue = dealer.getHandValue();
+
+        if (player.isBusted() && !dealer.isBusted()) {
+            System.out.println("딜러 승!");
+        } else if (dealer.isBusted() && !player.isBusted()) {
+            System.out.println("플레이어 승!");
+        } else if (playerValue > dealerValue) {
+            System.out.println("플레이어 승!");
+        } else if (playerValue < dealerValue) {
+            System.out.println("딜러 승!");
+        } else {
+            System.out.println("무승부!");
         }
     }
 }
