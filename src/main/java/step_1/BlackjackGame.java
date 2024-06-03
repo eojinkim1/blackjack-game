5de950e52e4ec240ae9e5cbe888fec916a8b622b
eojinkim1 <rladjwls999@gmail.com>
2024-06-03 22:54:52 +0900

    재시작 메소드 생성 및 게임 로직 변경

--- src/main/java/step_1/BlackjackGame.java
+++ src/main/java/step_1/BlackjackGame.java
@@ -1,23 +1,32 @@
 package step_1;
 
 import java.util.Scanner;
+
 public class BlackjackGame {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
     private Deck deck = new Deck();
     private Player player = new Player();
     private Dealer dealer = new Dealer();
 
+
     public static void main(String[] args) {
         BlackjackGame game = new BlackjackGame();
-        game.startGame();
+        while (true) {
+            game.startGame();
+            if (!game.askRestart()){
+                break;
+            }
+        }
     }
+
     public void startGame() {
-        System.out.println("게임을 시작합니다.");
         dealCards();
         statusDisplay();
 
-        play();
+        playerTurn();
 
-        if (!player.isBusted()){
+        if (!player.isBusted()) {
             dealer.drawCard(deck);
         }
 
@@ -37,22 +46,16 @@ public class BlackjackGame {
         System.out.println("딜러 카드 : " + dealer.toStringWithHiddenCard());
     }
 
-    private void play() {
-        Scanner scanner = new Scanner(System.in);
+    private void playerTurn() {
         while (true) {
             System.out.println("행동을 결정해주세요. Hit or Stay? (H/S): ");
-            String action = scanner.next().toUpperCase();
+            String action = SCANNER.next().toUpperCase();
 
             if (action.equals("H")) {
                 player.addCard(deck.drawCard());
+                statusDisplay();
                 if (player.isBusted()) {
                     System.out.println("플레이어 버스트!");
-                    break;
-                }
-                dealer.drawCard(deck);
-                    statusDisplay();
-                if (dealer.isBusted()){
-                    System.out.println("딜러 버스트!");
                     break;
                 }
             } else if (action.equals("S")) {
@@ -64,20 +67,24 @@ public class BlackjackGame {
     }
 
     private void showFinalHands() {
+        System.out.println("====================");
         System.out.println("카드를 오픈합니다.");
+        System.out.println("====================");
         System.out.println("플레이어 카드 : " + player);
         System.out.println("플레이어 카드 합계: " + player.getHandValue());
+        System.out.println("====================");
         System.out.println("딜러 카드 : " + dealer);
         System.out.println("딜러 카드 합계: " + dealer.getHandValue());
+        System.out.println("====================");
     }
 
     private void determineWinner() {
         int playerValue = player.getHandValue();
         int dealerValue = dealer.getHandValue();
 
-        if (player.isBusted() && !dealer.isBusted()) {
+        if (player.isBusted()) {
             System.out.println("딜러 승!");
-        } else if (dealer.isBusted() && !player.isBusted()) {
+        } else if (dealer.isBusted()) {
             System.out.println("플레이어 승!");
         } else if (playerValue > dealerValue) {
             System.out.println("플레이어 승!");
@@ -86,5 +93,28 @@ public class BlackjackGame {
         } else {
             System.out.println("무승부!");
         }
+    }
+
+    private boolean askRestart() {
+        while (true) {
+            System.out.println("\n게임을 다시 시작하시겠습니까? ( Y / N )");
+            String action = SCANNER.next().toUpperCase();
+
+            if (action.equals("Y")) {
+                System.out.println("새로운 게임을 시작합니다!");
+                resetGame();
+                return true;
+            } else if (action.equals("N")) {
+                System.out.println("게임이 종료되었습니다.");
+                return false;
+            } else {
+                System.out.println("잘못된 입력입니다. 'Y'를 입력하여 재시작하거나 'N'를 입력하여 게임을 종료하세요.");
+            }
+        }
+    }
+    private void resetGame(){
+        deck.resetDeck();
+        player = new Player();
+        dealer = new Dealer();
     }
 }
